fdaf05c657ed1733c751fc280a5e7233d3ac601c
Brian Bauska <brianbauska@gmail.com>
2026-03-19 13:00:10 -0700

    Update classie.js

--- js/classie.js
+++ js/classie.js
@@ -1,21 +1,17 @@
-/*!
+/* classie.js of sidebar-menu/js for sidebar.bauska.org
  * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
  * 
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
  * classie.toggle( elem, 'my-class' )
  */
-
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
 ( function( window ) {
 
 'use strict';
-
-// class helper functions from bonzo https://github.com/ded/bonzo
 
 function classReg( className ) {
   return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
